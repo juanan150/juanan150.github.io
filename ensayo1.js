f0784bdab365ae5bc6aae6a36174ae24990bc75c
juan.quiroz <juan.quiroz@gluky.com>
2021-06-24 19:06:54 -0500

    Primera version del sitio web personal

---
+++ ensayo1.js
@@ -0,0 +1,28 @@
+/*function count(str, char) {
+    let c = 0
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == char) {
+            c += 1
+        }
+    }
+    return c
+}
+
+alert(count("Hellohhhh dfh", "h"))
+
+function filter(arr){
+    return arr.filter(num => num > 10)
+
+  }
+
+  console.log(filter([1,2,3,4,5,10]))
+
+
+
+function hypotenuse(a, b) {
+    return Math.sqrt(a ** 2 + b ** 2)
+}
+
+console.log(hypotenuse(10,13))
+*/

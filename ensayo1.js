b886f55fd82c8cf2fa891edc53a785867db984ce
Juan Andres Quiroz <juanan0309@gmail.com>
2021-06-24 21:01:40 -0500

    arreglo de css

--- ensayo1.js
+++ ensayo1.js
@@ -1,28 +1,24 @@
-/*function count(str, char) {
-    let c = 0
-    str = str.toLowerCase()
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == char) {
-            c += 1
-        }
+function count(str, char) {
+  let c = 0;
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == char) {
+      c += 1;
     }
-    return c
+  }
+  return c;
 }
 
-alert(count("Hellohhhh dfh", "h"))
+console.log(count("hola gagts", "f"));
 
-function filter(arr){
-    return arr.filter(num => num > 10)
+function filter(arr) {
+  return arr.filter((num) => num > 10);
+}
 
-  }
-
-  console.log(filter([1,2,3,4,5,10]))
-
-
+console.log(filter([1, 2, 3, 4, 5, 10, 14, 15, 26, 78]));
 
 function hypotenuse(a, b) {
-    return Math.sqrt(a ** 2 + b ** 2)
+  return Math.sqrt(a ** 2 + b ** 2);
 }
 
-console.log(hypotenuse(10,13))
-*/
+console.log(hypotenuse(6, 8));
